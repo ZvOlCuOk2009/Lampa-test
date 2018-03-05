bf1035e54eccc03a0c26829d3cd0c38c54b3246c
Александр <ca.dev@datalink.ua>
2018-03-05 18:22:58 +0200

    Initial Commit

---
+++ Lampa test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lampa test
+//
+//  Created by Admin on 05.03.18.
+//  Copyright © 2018 Tsvigun Aleksander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lampa test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lampa test
+//
+//  Created by Admin on 05.03.18.
+//  Copyright © 2018 Tsvigun Aleksander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
